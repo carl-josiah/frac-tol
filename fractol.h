c160eb96fd3b87c0c7597b773416c278433400ba
carl-josiah <carl_josiah@yahoo.com>
2025-05-30 20:42:09 +0000

    structure with no makefile, as usual

---
+++ fractol.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccastro <ccastro@student.42abudhabi.ae>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/30 20:40:00 by ccastro           #+#    #+#             */
+/*   Updated: 2025/05/30 20:41:34 by ccastro          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+# define FRACTOL_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <math.h>
+
+#endif
